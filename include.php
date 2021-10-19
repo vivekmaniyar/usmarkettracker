02636160c3b1578f5dd0ccc9ebd9ef96e7bf0478
Vivek Maniyar <65184381+vivekmaniyar@users.noreply.github.com>
2021-10-19 19:01:03 +0530

    Update include.php

--- include.php
+++ include.php
@@ -6,6 +6,6 @@ $cryptos=mysqli_query($conn, "SELECT * FROM cryptolist");
 $stocks=mysqli_query($conn, "SELECT * FROM stocklist");
 
 
-$_SESSION["apiKey"] = "3G3I4AW8Q66N8ZS0";
+$_SESSION["apiKey"] = "api-key";
 
 ?>
